e456f483b5835cbdca3be28ffd6992f34af50bd9
anderson-souz4 <andersondesouzalopes69@gmail.com>
2023-04-21 04:25:36 -0300

    Modelagem da entidade usuario, criação do controller de usuario, repositório de usuario e usuarioDTO, alteração na serviceImpl

---
+++ src/main/java/com/github/anderson/vendas/domain/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.github.anderson.vendas.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    @NotEmpty(message = "{campo.login.obrigatorio}")
+    private String login;
+    @Column
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
+    private String senha;
+    @Column
+    private boolean admin;
+
+
+}
